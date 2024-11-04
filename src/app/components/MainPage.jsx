75933d26147846cad8df5d090eb7b0ce9b32026f
Aly Elhady <113694053+alyelhady9@users.noreply.github.com>
2024-11-05 00:17:13 +0200

    MainPage.jsx

--- src/app/components/MainPage.jsx
+++ src/app/components/MainPage.jsx
@@ -12,8 +12,10 @@ const MainPage = () => {
               <div className="w-full relative">
 
                 <div className="">
-                    <img className="w-full h-full max-md:hidden" src={bg} alt="Logo"  />
-                    <img className="w-full  hidden max-md:block" src={bgf} alt="Logo"  />
+                    <Image className="w-full layout="fill"
+            objectFit="cover" h-full max-md:hidden" src={bg} alt="Logo"  />
+                    <Image className="w-full layout="fill"
+            objectFit="cover"  hidden max-md:block" src={bgf} alt="Logo"  />
                     <div className="absolute top-1/4 w-full  mt-8  flex flex-col justify-center items-center px-10 ">
                         <p className="text-3xl max-md:text-xl font-bold mb-2 text-amber-500 ">
                           Find, Cook, Enjoy!
