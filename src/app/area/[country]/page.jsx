466f63696653291292fd9f508a91fb88bfecd434
Aly Elhady <113694053+alyelhady9@users.noreply.github.com>
2024-11-04 23:56:32 +0200

    page.jsx

--- src/app/area/[country]/page.jsx
+++ src/app/area/[country]/page.jsx
@@ -49,13 +49,7 @@ const CountryPage = (props) => {
     );
 }
 
-export async function generateStaticParams() {
-    const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
-    const data = await response.json();
 
-    return data.meals.map(country => ({
-        params: { country: country.strArea },
-    }));
 }
 
 export default CountryPage;
