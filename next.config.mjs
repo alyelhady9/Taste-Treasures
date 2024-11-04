7b52d75e8895e316495d5ce90fa99d47f4ff457a
unknown <alyelhady10@gmail.com>
2024-11-04 23:44:16 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        loader: 'akamai',
+        path: '',
+      },
+      assetPrefix: './',
+    
 };
 
 module.exports = {
