b3c17f840312ec50d621a24ede3f60b5432ea4a8
unknown <alyelhady10@gmail.com>
2025-08-04 00:26:57 +0300

    Fix: Corrected font import and installed Geist package

--- src/app/search/page.jsx
+++
@@ -1,9 +0,0 @@
-import SearchMeals from "../components/SearchBar";
-
-const SearchPage = () => {
-    return ( <div className="w-full flex justify-center ">
-        <SearchMeals />
-    </div> );
-}
- 
-export default SearchPage;
